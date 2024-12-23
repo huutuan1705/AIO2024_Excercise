886836b37785b512770f259670aabac70f721e66
tuan.nh <tuan.nh@tinasoft.com.vn>
2024-12-23 10:06:37 +0700

    update ex 4

---
+++ Module6/Week4/vision_transformer/model.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+import math
+import os
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, embed_dim, num_heads, ff_dim, dropput=0.1):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            batch_first=True
+        )
+        
+        self.ffn = nn.Sequential(
+            nn.Linear(in_features=embed_dim, out_features=ff_dim, bias=True),
+            nn.ReLU(),
+            nn.Linear(in_features=ff_dim, out_features=embed_dim, bias=True)
+        )
+        
+        self.layernorm_1 = nn.LayerNorm(normalized_shape=embed_dim, eps=1e-6)
+        self.layernorm_2 = nn.LayerNorm(normalized_shape=embed_dim, eps=1e-6)
+        self.dropout_1 = nn.Dropout(p=dropput)
+        self.dropout_2 = nn.Dropout(p=dropput)
+        
+    def forward(self, query, key, value):
+        attn_output, _ = self.attn(query, key, value)
+        attn_output = self.dropout_1(attn_output)
+        out_1 = self.layernorm_1(query + attn_output)
+        ffn_output = self.ffn(out_1)
+        ffn_output = self.dropout_2(ffn_output)
+        out_2 = self.layernorm_2(out_1 + ffn_output)
+        
+        return out_2
