4f96d78f59858ea11cb33e843b436c0cb65b82ac
tuan.nh <tuan.nh@tinasoft.com.vn>
2024-12-28 08:14:24 +0700

    update excercise

--- Module6/Week4/bert_text_cls/main.py
+++ Module6/Week4/bert_text_cls/main.py
@@ -1,5 +1,7 @@
+import numpy as np
+import evaluate
 
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, TrainingArguments, Trainer
 from datasets import load_dataset
 
 ds = load_dataset('thainq07/ntc-scv')
@@ -18,6 +20,42 @@ def preprocess_function(examples):
 
     return result
 
-precessed_dataset = ds.map(
+processed_dataset = ds.map(
     preprocess_function, batched=True, desc="Runiing tokenizer on dataset"
+)
+
+num_labels = 2
+config = AutoConfig.from_pretrained(
+    model_name, num_labels=num_labels, finetuning_task="text-classification"
+)
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_name, config=config
+)
+
+metric = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    result = metric.compute(predictions=predictions, references=labels)
+    return result
+
+training_args = TrainingArguments(
+    output_dir="save_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=128,
+    per_device_eval_batch_size=128,
+    num_train_epochs=10,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=processed_dataset["train"],
+    eval_dataset=processed_dataset["valid"],
+    compute_metrics=compute_metrics,
+    tokenizer=tokenizer
 )
